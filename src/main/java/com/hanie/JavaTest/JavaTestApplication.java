c2147cfa795c8e1fe40a071b4be2426efb8d7275
omid merikhi <omidi.merikhi1999@gmail.com>
2023-04-08 11:34:12 +0330

    create project and add commanline runner:)

---
+++ src/main/java/com/hanie/JavaTest/JavaTestApplication.java
@@ -0,0 +1,22 @@
+package com.hanie.JavaTest;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class JavaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTestApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner omidi(){
+		return args -> {
+			System.out.println("my name is omid:)");
+		};
+	}
+
+}
